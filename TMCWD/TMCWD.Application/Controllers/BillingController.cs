b553eebe371b48986f032f0f6e8cda1297b15596
Wally Villanueva <wallyvillanueva78@gmail.com>
2026-07-31 10:25:28 +0800

    resolve issues after merge

--- TMCWD/TMCWD.Application/Controllers/BillingController.cs
+++ TMCWD/TMCWD.Application/Controllers/BillingController.cs
@@ -63,31 +63,6 @@ namespace TMCWD.Application.Controllers
             return View(model);
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult SubmitAdjustment(BillAdjustmentViewModel model)
-        {
-            if (!ModelState.IsValid)
-            {
-                model.BamDate ??= DateTime.Today;
-                model.MeterReaders = new List<SelectListItem>
-                {
-                    new SelectListItem { Value = "MR001", Text = "Juan Dela Cruz" },
-                    new SelectListItem { Value = "MR002", Text = "Maria Santos" },
-                    new SelectListItem { Value = "MR003", Text = "Pedro Reyes" }
-                };
-                model.RemarksOptions = new List<SelectListItem>
-                {
-                    new SelectListItem { Value = "Meter Error",   Text = "Meter Error" },
-                    new SelectListItem { Value = "Reading Error", Text = "Reading Error" },
-                    new SelectListItem { Value = "System Error",  Text = "System Error" },
-                    new SelectListItem { Value = "Other",         Text = "Other" }
-                };
-                return View("BillAdjustment", model);
-            }
-            return View("BillAdjustment");
-        }
-
         public IActionResult Index() => View();
         public IActionResult PenaltyCharging() => View();
         public IActionResult Penalty() => View();
@@ -100,7 +75,10 @@ namespace TMCWD.Application.Controllers
         }
 
         public async  Task<IActionResult> OtherCharges(DateTime billPeriod)
-        public IActionResult ReadingSheet()
+        {
+            return Ok();
+        }
+        public async Task<IActionResult> ReadingSheet(DateTime billPeriod)
         {
             var allBillings = await _billingTrans.GetAll() ?? new List<Model.Billing.Interfaces.BillingBase>();
             var matching = allBillings.Where(b => b.BillingPeriod.Date == billPeriod.Date).ToList();
@@ -129,37 +107,6 @@ namespace TMCWD.Application.Controllers
             }
 
             return Ok(result);
-        }
-
-        public IActionResult BillAdjustment()
-        {
-            var model = new BillAdjustmentViewModel
-            {
-                BamDate = DateTime.Today,
-                MeterReaders = new List<SelectListItem>
-                {
-                    new SelectListItem { Value = "MR001", Text = "Juan Dela Cruz" },
-                    new SelectListItem { Value = "MR002", Text = "Maria Santos" },
-                    new SelectListItem { Value = "MR003", Text = "Pedro Reyes" }
-                },
-                RemarksOptions = new List<SelectListItem>
-                {
-                    new SelectListItem { Value = "Meter Error", Text = "Meter Error" },
-                    new SelectListItem { Value = "Reading Error", Text = "Reading Error" },
-                    new SelectListItem { Value = "System Error", Text = "System Error" },
-                    new SelectListItem { Value = "Other", Text = "Other" }
-                },
-                AdjustmentLines = new List<AdjustmentLineItem>
-                {
-                    new AdjustmentLineItem { Key = "usage", Label = "Usage", HasAdjustmentColumn = true, IsChecked = false },
-                    new AdjustmentLineItem { Key = "currentBill", Label = "Current Bill", HasAdjustmentColumn = true, IsChecked = false },
-                    new AdjustmentLineItem { Key = "penalty", Label = "Penalty", HasAdjustmentColumn = true, IsChecked = false },
-                    new AdjustmentLineItem { Key = "present", Label = "Present", HasAdjustmentColumn = false, IsChecked = false },
-                    new AdjustmentLineItem { Key = "previous", Label = "Previous", HasAdjustmentColumn = false, IsChecked = false }
-                }
-            };
-
-            return View(model);
         }
 
         [HttpPost]
@@ -193,6 +140,9 @@ namespace TMCWD.Application.Controllers
         }
 
         public async Task<IActionResult> GetBillById(int id)
+        {
+            return Ok();
+        }
         public class WaivePenaltiesRequest
         {
             public List<string> BillingReferenceIds { get; set; } = new();
